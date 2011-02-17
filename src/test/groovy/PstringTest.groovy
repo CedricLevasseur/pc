3674aa4abc7b4734900e8142245eaf844c45709f
Cedric Levasseur <cedric.levasseur@gmail.com>
2011-02-17 09:08:59 +0100

    Correcting a test

--- src/test/groovy/PstringTest.groovy
+++ src/test/groovy/PstringTest.groovy
@@ -12,14 +12,13 @@ class PstringTest extends GroovyTestCase{
 	}
 	public void testVariationNumber(){
 
-	Pstring p1 = new Pstring()
-	ArrayList<String> r1 = new ArrayList<String>()
-	r1=p1.variationNumber(23,p1.VARIATION_NUMBER)
-	assert r1.size() == p1.VARIATION_NUMBER
-	//assert [23..28].equals(r1)
-	/*for(Integer i in 23..28){
-		assert i in r1		
-	}*/
+		Pstring p1 = new Pstring()
+		ArrayList<String> r1 = new ArrayList<String>()
+		r1=p1.variationNumber(23,p1.VARIATION_NUMBER)
+		assert r1.size() == p1.VARIATION_NUMBER
+		for(Integer i in 23..27){
+			assert i.toString() in r1		
+		}
 	}
 
 	public void testCapitalize(){
